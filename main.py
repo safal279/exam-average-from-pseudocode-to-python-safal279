bef7fd58871631109ae7b3dab19d4c1835b01451
IS250 Student <changeme@example.com>
2025-11-17 22:18:31 -0600

    added coding added pseudo codes test results

--- main.py
+++ main.py
@@ -4,7 +4,21 @@ Class-Section: IS 250-01
 Assignment Title: PyCharm and GitHub Setup Activity
 Submission Date: 11 - 17 - 2025
 """
+"""
+Write your pseudocode here.
+Step 1: Asking the user to enter the first score
+Step 2: Asking the user to enter the second score
+Step 3: Asking the user to enter the third score
+Step 4: Calculating the score average using a function 
+Step 5: Add the 3 scores together
+Step 6: Divide the sum by 3 to get the average 
+Step 7: Returning the average 
+Step 8: Print or we can say display each score
+Step 9: Print or display the average score
 
+Do not write any Python code in this section.
+Your pseudocode should describe your overall approach in your own words.
+"""
 # Your Python code begins below this line.
 
 
@@ -34,7 +48,6 @@ print("Third score: ", third)
 
 #Display the average
 print("The average score is: ", result)
-
 
 
 
